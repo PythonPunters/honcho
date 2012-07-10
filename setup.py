0a8724a002adeb8cfd660ef5cebb5f7e5a3afa1c
Kiall Mac Innes <kiall@managedit.ie>
2012-07-10 12:21:39 +0100

    Update setup.py to reflect fork.

--- setup.py
+++ setup.py
@@ -3,19 +3,23 @@ from setuptools import setup, find_packages
 from honcho import __version__
 
 setup(
-    name = 'honcho',
+    name = 'redpie.honcho',
     version = __version__,
     packages = find_packages(),
 
     # metadata for upload to PyPI
-    author = 'Nick Stenning',
-    author_email = 'nick@whiteink.com',
-    url = 'https://github.com/nickstenning/honcho',
-    description = 'Honcho: a python clone of Foreman. For managing Procfile-based applications.',
+    author = 'Kiall Mac Innes',
+    author_email = 'kiall.macinnes@redpie.com',
+    url = 'https://github.com/redpie/honcho',
+    description = 'Fork of http://pypi.python.org/pypi/honcho',
     license = 'MIT',
     keywords = 'sysadmin process procfile',
 
     test_suite='test.test_simple',
+
+    install_requires=[
+        'colorama'
+    ],
 
     entry_points = {
         'console_scripts': [
